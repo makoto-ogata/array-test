9eb9e907db47164cba5a27ebee0a929e5e1db0d7
緒方誠 <ogatamakoto1976@gmail.com>
2016-09-24 19:51:30 +0900

    add function

--- js/index.js
+++ js/index.js
@@ -4,13 +4,13 @@ function putElements (){
 		url: 'params.json',
 		dataType: 'json',
 		success: function(result) {
-			console.log(result);
 			// 8〜10行目まで関数にする
-			$.each( result, function(index, elem){
-					$('.text-list').append('<li style="color:'+ elem.color +'">' + elem.text + '</li>');
-			});
-			//var list1 = $('<li style="color:'+ elem.color +'">' + elem.text + '</li>').appendTo('.text-list');
-			//$(list1).addClass('test');
+			function trigger (){
+				$.each( result, function(index, elem){
+						$('.text-list').append('<li style="color:'+ elem.color +'">' + elem.text + '</li>');
+				});
+			}
+			trigger();
 		},
 		error: function(){
 			alert('読み込めませんでした');
